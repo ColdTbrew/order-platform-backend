192fe458632357e64a64cc90b0813b3032568e65
ColdTbrew <shchoi8687@hotmail.com>
2025-08-10 01:28:38 +0900

    RedisConnectionFactory 빈을 찾지 못해 발생하던 오류가 해

--- src/test/java/app/global/config/TestRedissonConfig.java
+++ src/test/java/app/global/config/TestRedissonConfig.java
@@ -6,16 +6,23 @@ import org.redisson.config.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
 
 @TestConfiguration
 @Profile("test")
 public class TestRedissonConfig {
 
-    @Bean
+    @Bean(destroyMethod = "shutdown")
     public RedissonClient redissonClient() {
         Config config = new Config();
         config.useSingleServer()
               .setAddress("redis://localhost:6379"); // 테스트용 Redis 주소 명시
         return Redisson.create(config);
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(RedissonClient redissonClient) {
+        return new RedissonConnectionFactory(redissonClient);
     }
 }
