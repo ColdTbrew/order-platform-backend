e9dfcfc9111dd023b8fd2cc294e6a318a3d656e8
ColdTbrew <shchoi8687@hotmail.com>
2025-08-10 01:36:18 +0900

    Redis 관련 빈 의존성 문제

--- src/test/java/app/global/config/TestRedissonConfig.java
+++ src/test/java/app/global/config/TestRedissonConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.redisson.spring.data.connection.RedissonConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @TestConfiguration
 @Profile("test")
@@ -24,5 +26,16 @@ public class TestRedissonConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory(RedissonClient redissonClient) {
         return new RedissonConnectionFactory(redissonClient);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+        return template;
     }
 }
