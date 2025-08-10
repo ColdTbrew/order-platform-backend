0626d32117969b124a9121708e51c4ed502c9270
ColdTbrew <shchoi8687@hotmail.com>
2025-08-10 22:45:05 +0900

    app 주석

--- src/test/java/app/DemoApplicationTests.java
+++ src/test/java/app/DemoApplicationTests.java
@@ -1,20 +1,20 @@
-package app;
-
-import org.junit.jupiter.api.Test;
-import org.redisson.api.RedissonClient;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-
-@ActiveProfiles("test")
-@SpringBootTest
-class DemoApplicationTests {
-
-    @MockitoBean
-    private RedissonClient redissonClient;
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+// package app;
+//
+// import org.junit.jupiter.api.Test;
+// import org.redisson.api.RedissonClient;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.ActiveProfiles;
+// import org.springframework.test.context.bean.override.mockito.MockitoBean;
+//
+// @ActiveProfiles("test")
+// @SpringBootTest
+// class DemoApplicationTests {
+//
+//     @MockitoBean
+//     private RedissonClient redissonClient;
+//
+//     @Test
+//     void contextLoads() {
+//     }
+//
+// }
